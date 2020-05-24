2b0e6b5b7173137698398dc81e1df334f102eda4
LongYinan <longyinan.brooklyn@bytedance.com>
2020-05-24 13:40:01 +0800

    wip [skip ci]

--- packages/argon2/src/lib.rs
+++ packages/argon2/src/lib.rs
@@ -3,7 +3,7 @@ extern crate napi_rs as napi;
 #[macro_use]
 extern crate napi_rs_derive;
 
-use napi::{Env, Object, Result, Value};
+use napi::{Buffer, CallContext, Env, Object, Result, Value};
 
 #[cfg(unix)]
 #[global_allocator]
@@ -13,4 +13,12 @@ register_module!(argon2, init);
 
 fn init(env: &Env, exports: &mut Value<Object>) -> Result<()> {
   Ok(())
+}
+
+#[js_function(2)]
+fn hash(ctx: CallContext) -> Result<Value<Buffer>> {
+  let input = ctx.get::<Buffer>(0)?;
+  let options = ctx.get::<Object>(1)?;
+
+  ctx.env.create_buffer_with_data(vec![])
 }
